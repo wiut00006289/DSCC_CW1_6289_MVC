60efa615f4145f552fa74d87299e0df0b3bb8083
wiut00006289 <drikhsiboev@students.wiut.uz>
2020-10-30 20:20:35 +0500

    My First Commit

---
+++ DSCC_CW1_6289_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DSCC_CW1_6289_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
